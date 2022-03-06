d75191c773f8d7731e66fdefd8db453284700973
Ryan <rmthomas1998@gmail.com>
2022-03-06 13:11:57 -0500

    analytics

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,20 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-NGYV38LDGC`}
+      ></Script>
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag() {dataLayer.push(arguments);}
+        gtag("js", new Date());
+
+        gtag("config", "G-NGYV38LDGC");`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
